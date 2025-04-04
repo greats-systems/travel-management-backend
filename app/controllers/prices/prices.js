914485a6bf59115d29757566efd37952977d9907
Ngqabutho-Moyo <your.moyongqaa@gmail.com>
2025-04-04 15:11:14 +0200

    1st commit

--- app/controllers/prices/prices.js
+++ app/controllers/prices/prices.js
@@ -10,16 +10,35 @@ const amadeus = new Amadeus({
 });
 
 export default async function getPrices(request, response) {
-  try {
-    const apiResponse = await amadeus.shopping.flightOffersSearch.get({
-      originLocationCode: request.body.origin,
-      destinationLocationCode: request.body.destination,
-      departureDate: request.body.departureDate,
-      adults: request.body.adults,
-      max: 30
-    });
-    response.status(200).send(apiResponse.data);
-  } catch (error) {
-    console.error("Error:", error);
+  if (request.body.returnDate != null) {
+    try {
+      const apiResponse = await amadeus.shopping.flightOffersSearch.get({
+        originLocationCode: request.body.origin,
+        destinationLocationCode: request.body.destination,
+        departureDate: request.body.departureDate,
+        returnDate: request.body.returnDate,
+        adults: request.body.adults,
+        // children: request.body.children,
+        // infants: request.body.infants,
+        // travelClass: request.body.cabin,
+        // max: 1,
+      });
+      response.status(200).send(apiResponse.data);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  } else {
+    try {
+      const apiResponse = await amadeus.shopping.flightOffersSearch.get({
+        originLocationCode: request.body.origin,
+        destinationLocationCode: request.body.destination,
+        departureDate: request.body.departureDate,
+        adults: request.body.adults,
+        // max: 1,
+      });
+      response.status(200).send(apiResponse.data);
+    } catch (error) {
+      console.error("Error:", error);
+    }
   }
 }
